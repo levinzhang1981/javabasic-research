ac3cb1cfc3544c41c224075e7ae52c30d973938b
levinzhang1981 <levinzhang1981@gmail.com>
2015-03-24 21:11:18 +0800

    Memory

---
+++ JavaBasicNIO/src/com/levinzhang/A.java
@@ -0,0 +1,14 @@
+package com.levinzhang;
+
+public class A {
+	private int i;
+
+	public int getI() {
+		return i;
+	}
+
+	public void setI(int i) {
+		this.i = i;
+	}
+	
+}
--- JavaBasicNIO/src/com/levinzhang/Test.java
+++ JavaBasicNIO/src/com/levinzhang/Test.java
@@ -1,5 +1,10 @@
 package com.levinzhang;
 
 public class Test {
-
+	public static  final A a = new  A();
+	
+	public static void main(String[] args) {
+		a.setI(111);
+		System.out.println(a.getI());
+	}
 }
